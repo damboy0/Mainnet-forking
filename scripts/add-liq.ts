d489637ce86e4fa44b183925bedb945e3c7f6744
damboy0 <fagbolade006@gmail.com>
2024-09-13 15:37:53 +0100

    Initial Commit

---
+++ scripts/add-liq.ts
@@ -0,0 +1,14 @@
+import { ethers } from "hardhat";
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+
+async function main() {
+
+    
+}
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
